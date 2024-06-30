331eaef009736f84d350f8e5754444fcc3cceb5e
mangat-ram <se.mangat.ram@gmail.com>
2024-06-30 17:04:31 +0530

    Fixed issue of HomeVar

--- src/context/ActiveSectionContext.tsx
+++ src/context/ActiveSectionContext.tsx
@@ -19,7 +19,7 @@ export const ActiveSectionContext = createContext<ActiveSectionContextType | nul
 export default function ActiveSectionContextProvider(
   {children}: ActiveSectionContextProviderProps
 ){
-  const [ activeSection, setActiveSection ] = useState<SectionName>("Home");
+  const [ activeSection, setActiveSection ] = useState<SectionName>("About Us");
   const [ timeOfLastClick, setTimeOfLastClick] = useState(0);
 
   return (
